36e8d0ac5557a33f0d8c30b17e247bd0cc3de47b
Lucas-Barret <69370335+Lucas-Barret@users.noreply.github.com>
2025-04-13 12:51:36 +0400

    feat: add farm

---
+++ app/views/farms/_farm.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! farm, :id, :name, :producer_id, :created_at, :updated_at
+json.url farm_url(farm, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :farms
   resources :roasters
   resources :coffees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
